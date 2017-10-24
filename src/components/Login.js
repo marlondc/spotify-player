215627de47d8f5303cc1e0fba8ba6a434116e8d0
Marlon DC <marlondc@thisismn.com>
2017-10-24 17:16:09 +0100

    style login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const Login = props => (
-  <div className="login">
-    <a href="http://localhost:8000/login" onClick={() => props.login()}>LOGIN</a>
+  <div className="container">
+    <div className="top top--login">
+      <div className="content content--login">
+        <div className="input input--login">
+          <a className="input__add-button input__add-button--login" href="http://localhost:8000/login" onClick={() => props.login()}>LOGIN</a>
+        </div>
+      </div>
+    </div>
+    <div className="bottom bottom--login" />
   </div>
 );
 
